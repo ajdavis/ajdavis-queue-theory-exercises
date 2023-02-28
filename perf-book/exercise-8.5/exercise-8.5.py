42783a63bde998915ac822150c0aa59e7f0367cc
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-02-28 09:31:14 -0500

    fix exercise-8.5.py

--- perf-book/exercise-8.5/exercise-8.5.py
+++ perf-book/exercise-8.5/exercise-8.5.py
@@ -5,9 +5,13 @@ def trial():
     x, y, i = 0, 0, 0
     while True:
         if random.randint(0, 1) == 0:
-            x = random.randint(0, 7)
+            # Move to a random new x, can't stay at the same x.
+            new_x = random.randint(0, 6)
+            x = new_x + 1 if new_x >= x else new_x
         else:
-            y = random.randint(0, 7)
+            # Move to a random new y, can't stay at the same y.
+            new_y = random.randint(0, 6)
+            y = new_y + 1 if new_y >= y else new_y
 
         if x == y == 7:
             return i
@@ -15,14 +19,14 @@ def trial():
         i += 1
 
 
-n = int(1e5)
-trial_sum, trial_squared_sum = 0, 0
+n = int(1e6)
+T_sum, T_squared_sum = 0, 0
 for _ in range(n):
     T = trial()
-    trial_sum += T
-    trial_squared_sum += T * T
+    T_sum += T
+    T_squared_sum += T * T
 
-expected_value = trial_sum / n
-variance = trial_squared_sum / n - expected_value * expected_value
-# Approximately 78.7 and 6013.
+expected_value = T_sum / n
+variance = T_squared_sum / n - expected_value * expected_value
+# Approximately 69.03 and 4632.9.
 print(f"E[T] = {expected_value}, Var(T) = {variance}")
